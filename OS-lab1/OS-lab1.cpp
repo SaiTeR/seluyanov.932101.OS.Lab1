54760162c635c06f566d8c837f6351614bc638c2
Danila Seluyanov <brden2000@gmail.com>
2023-12-26 23:42:51 +0300

    Add files via upload

---
+++ OS-lab1/OS-lab1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+
+mutex mtx;
+condition_variable cv;
+bool eventSent = false;
+
+
+void producer() {
+    unique_lock<mutex> lock(mtx);
+
+    while (eventSent) {
+        cv.wait(lock);
+    }
+
+    cout << "[ПОСТАВЩИК]: \tсобытие отправлено" << endl;
+    eventSent = true;
+    
+    cv.notify_one();
+}
+
+void consumer() {
+    unique_lock<mutex> lock(mtx);
+
+    while (!eventSent) {
+        cv.wait(lock);
+    }
+
+    cout << "[ПОТРЕБИТЕЛЬ]: \tсобытие получено" << endl;
+    eventSent = false;
+
+    cv.notify_one();
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    thread producerThread(producer);
+    thread consumerThread(consumer);
+
+    producerThread.join();
+    consumerThread.join();
+
+    return 0;
+}
